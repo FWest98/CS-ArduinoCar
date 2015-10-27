fbf6088f7bcb690f53b9eedae054b768eef14a83
Thomas <denhollander.thomas@gmail.com>
2015-10-27 14:39:05 +0100

    First commit
    Signed-off-by: Thomas <denhollander.thomas@gmail.com>

---
+++ app/src/main/java/com/thomasdh/arduinocar/BluetoothDevicePicker.java
@@ -0,0 +1,153 @@
+package com.thomasdh.arduinocar;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class BluetoothDevicePicker extends AppCompatActivity{
+
+    public static final String EXTRA_BLUETOOTH_DEVICE = "bluetooth_device";
+
+    class BluetoothDeviceAdapter extends RecyclerView.Adapter<BluetoothDeviceAdapter.BluetoothViewHolder> {
+        public class BluetoothViewHolder extends RecyclerView.ViewHolder {
+
+            TextView view;
+            BluetoothDevice bluetoothDevice;
+
+            public BluetoothViewHolder(TextView itemView) {
+                super(itemView);
+                this.view = itemView;
+            }
+
+            public void setDevice(BluetoothDevice device){
+                this.bluetoothDevice = device;
+                view.setText(device.getName());
+                view.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent data = new Intent();
+                        data.putExtra(EXTRA_BLUETOOTH_DEVICE, bluetoothDevice);
+                        if (getParent() == null) {
+                            setResult(Activity.RESULT_OK, data);
+                        } else {
+                            getParent().setResult(Activity.RESULT_OK, data);
+                        }
+                        finish();
+                    }
+                });
+            }
+        }
+
+        ArrayList<BluetoothDevice> devices;
+        LayoutInflater inflater;
+        Activity activity;
+
+        public BluetoothDeviceAdapter(Activity context) {
+            super();
+            activity = context;
+            devices = new ArrayList<>();
+            inflater = LayoutInflater.from(context);
+        }
+
+        public void addDevice(BluetoothDevice device){
+            devices.add(device);
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public int getItemCount() {
+            return devices.size();
+        }
+
+        @Override
+        public BluetoothViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            TextView view = (TextView) inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+            return new BluetoothViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(BluetoothViewHolder holder, int position) {
+            holder.setDevice(devices.get(position));
+        }
+    }
+
+    BluetoothDeviceAdapter adapter;
+    BluetoothAdapter bluetooth;
+    static final int REQUEST_ENABLE_BLUETOOTH = 11;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.layout_bluetooth_device_list);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.view_bluetooth_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        recyclerView.setHasFixedSize(true);
+        adapter = new BluetoothDeviceAdapter(this);
+        recyclerView.setAdapter(adapter);
+
+        bluetooth = BluetoothAdapter.getDefaultAdapter();
+        if (bluetooth == null) {
+            Toast.makeText(this, "Your device doesn't support Bluetooth", Toast.LENGTH_LONG).show();
+            finish();
+        } else {
+            if (!bluetooth.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BLUETOOTH);
+            } else {
+                startDeviceSearching(bluetooth);
+            }
+        }
+    }
+
+    private BroadcastReceiver mReceiver;
+
+    void startDeviceSearching(final BluetoothAdapter bluetooth) {
+        bluetooth.startDiscovery();
+
+        mReceiver = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    adapter.addDevice(device);
+                }
+            }
+        };
+
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mReceiver);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_ENABLE_BLUETOOTH) {
+            if (resultCode == RESULT_OK)
+                startDeviceSearching(bluetooth);
+            else
+                Toast.makeText(this, "You cancelled bluetooth enabling", Toast.LENGTH_LONG).show();
+        } else
+            super.onActivityResult(requestCode, resultCode, data);
+    }
+}
---
+++ app/src/main/java/com/thomasdh/arduinocar/MainActivity.java
@@ -0,0 +1,163 @@
+package com.thomasdh.arduinocar;
+
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+
+    BluetoothDevice device;
+    OutputStream connection;
+    BluetoothSocket socket;
+
+    static final int REQUEST_BLUETOOTH_DEVICE = 12;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Set listener
+        View.OnTouchListener listener = new ControlTouchListener();
+        Button up = (Button) findViewById(R.id.button_up);
+        up.setOnTouchListener(listener);
+        Button down = (Button) findViewById(R.id.button_down);
+        down.setOnTouchListener(listener);
+        Button left = (Button) findViewById(R.id.button_left);
+        left.setOnTouchListener(listener);
+        Button right = (Button) findViewById(R.id.button_right);
+        right.setOnTouchListener(listener);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (device == null)
+            startDevicePicker();
+        else if (connection == null)
+            setupConnection();
+    }
+
+    static final byte[] stop = hexStringToByteArray("00");
+    static final byte[] forward = hexStringToByteArray("0144");
+    static final byte[] left = hexStringToByteArray("0244");
+    static final byte[] backwards = hexStringToByteArray("0344");
+    static final byte[] right = hexStringToByteArray("0444");
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_BLUETOOTH_DEVICE) {
+            if (resultCode == RESULT_OK) {
+                device = data.getParcelableExtra(BluetoothDevicePicker.EXTRA_BLUETOOTH_DEVICE);
+                setupConnection();
+            } else
+                startDevicePicker();
+        } else
+            super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    void startDevicePicker() {
+        Intent i = new Intent(this, BluetoothDevicePicker.class);
+        startActivityForResult(i, REQUEST_BLUETOOTH_DEVICE);
+    }
+
+    static final UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    void setupConnection(){
+        if (device == null)
+            throw new RuntimeException("Called setupConnection while device was null");
+        new AsyncTask<BluetoothDevice, Void, BluetoothSocket>(){
+            @Override
+            protected BluetoothSocket doInBackground(BluetoothDevice[] params) {
+                try {
+                    return params[0].createRfcommSocketToServiceRecord(uuid);
+                } catch (Exception e) {
+                    return null;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(BluetoothSocket outputStream) {
+                socket = outputStream;
+                if (socket != null)
+                    try {
+                        connection = socket.getOutputStream();
+                    } catch (Exception e) {
+                        Log.e("MainActivity", "Could not get outputstream", e);
+                    }
+            }
+        }.execute(device);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (connection != null)
+            try {
+                connection.close();
+            } catch (Exception e) {
+                Log.e("MainActivity", "Could not close", e);
+            }
+        connection = null;
+        device = null;
+    }
+
+    class ControlTouchListener implements Button.OnTouchListener {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            if (connection == null)
+                return false;
+
+            try {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    switch (v.getId()) {
+                        case R.id.button_down:
+                            connection.write(backwards);
+                            connection.flush();
+                            break;
+                        case R.id.button_left:
+                            connection.write(left);
+                            connection.flush();
+                            break;
+                        case R.id.button_right:
+                            connection.write(right);
+                            connection.flush();
+                            break;
+                        case R.id.button_up:
+                            connection.write(forward);
+                            connection.flush();
+                            break;
+                    }
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    connection.write(stop);
+                    connection.flush();
+                }
+            } catch (Exception e) {
+                Log.e("MainActivity", "Could not send", e);
+            }
+            return false;
+        }
+    }
+
+    public static byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] data = new byte[len/2];
+
+        for(int i = 0; i < len; i+=2){
+            data[i/2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i+1), 16));
+        }
+
+        return data;
+    }
+}
